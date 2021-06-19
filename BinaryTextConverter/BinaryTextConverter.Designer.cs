9dc46cc2f66095a3cd3aba46f3343c5214fc5a2c
Melanie Sifen <melanie.sifen@doverfs.com>
2021-06-19 14:21:38 -0500

    inital commit

---
+++ BinaryTextConverter/BinaryTextConverter.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace BinaryTextConverter
+{
+    partial class BinaryTextConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.BinaryTextBox = new System.Windows.Forms.TextBox();
+            this.TextTextBox = new System.Windows.Forms.TextBox();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.ReverseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ConvertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConvertButton.Location = new System.Drawing.Point(633, 141);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(155, 42);
+            this.ConvertButton.TabIndex = 1;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = false;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+
+            // 
+            // BinaryTextBox
+            // 
+            this.BinaryTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.BinaryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinaryTextBox.Location = new System.Drawing.Point(44, 149);
+            this.BinaryTextBox.Name = "BinaryTextBox";
+            this.BinaryTextBox.ReadOnly = true;
+            this.BinaryTextBox.Size = new System.Drawing.Size(100, 25);
+            this.BinaryTextBox.TabIndex = 2;
+            this.BinaryTextBox.Text = "Binary:";
+            this.BinaryTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TextTextBox
+            // 
+            this.TextTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextTextBox.Location = new System.Drawing.Point(44, 231);
+            this.TextTextBox.Name = "TextTextBox";
+            this.TextTextBox.ReadOnly = true;
+            this.TextTextBox.Size = new System.Drawing.Size(100, 25);
+            this.TextTextBox.TabIndex = 3;
+            this.TextTextBox.Text = "Text:";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(164, 147);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(453, 26);
+            this.InputTextBox.TabIndex = 4;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.OutputTextBox.Location = new System.Drawing.Point(164, 233);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(453, 26);
+            this.OutputTextBox.TabIndex = 5;
+            // 
+            // ReverseButton
+            // 
+            this.ReverseButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ReverseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReverseButton.Location = new System.Drawing.Point(26, 299);
+            this.ReverseButton.Name = "ReverseButton";
+            this.ReverseButton.Size = new System.Drawing.Size(155, 40);
+            this.ReverseButton.TabIndex = 1;
+            this.ReverseButton.Text = "Reverse";
+            this.ReverseButton.UseVisualStyleBackColor = false;
+            this.ReverseButton.Click += new System.EventHandler(this.ReverseButton_Click);
+            // 
+            // BinaryTextConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReverseButton);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.TextTextBox);
+            this.Controls.Add(this.BinaryTextBox);
+            this.Controls.Add(this.ConvertButton);
+            this.Name = "BinaryTextConverter";
+            this.Text = "Binary Text Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.TextBox BinaryTextBox;
+        private System.Windows.Forms.TextBox TextTextBox;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Button ReverseButton;
+    }
+}
+
---
+++ BinaryTextConverter/BinaryTextConverter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryTextConverter
+{
+    public partial class BinaryTextConverter : Form
+    {
+        public BinaryTextConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reverseTextBoxes()
+        {
+            if (BinaryTextBox.Text == "Binary:")
+            {
+                BinaryTextBox.Text = "Text:";
+                TextTextBox.Text = "Binary:";
+            }
+            else
+            {
+                BinaryTextBox.Text = "Binary:";
+                TextTextBox.Text = "Text:";
+            }
+            InputTextBox.Text = OutputTextBox.Text;
+            OutputTextBox.Text = "";
+
+        }
+
+        private void ReverseButton_Click(object sender, EventArgs e)
+        {
+            reverseTextBoxes();
+        }
+        public Byte[] GetBytesFromBinaryString(String binary)
+        {
+            var list = new List<Byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                
+                String t = binary.Substring(i, 8);
+                
+                list.Add(Convert.ToByte(t, 2));
+            }
+
+            return list.ToArray();
+        }
+        bool HasNonASCIIChars(string str)
+        {
+            return (System.Text.Encoding.UTF8.GetByteCount(str) != str.Length);
+        }
+        public static byte[] ConvertToByteArray(string str, Encoding encoding)
+        {
+            return encoding.GetBytes(str);
+        }
+        public static String ToBinary(Byte[] data)
+        {
+            return string.Join(" ", data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+        }
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            if (BinaryTextBox.Text == "Binary:")
+            {
+                string userInput = InputTextBox.Text;
+                string[] subs = userInput.Split(' ');
+                string result = "";
+                foreach (var sub in subs)
+                {
+                    var data = GetBytesFromBinaryString(sub);
+                    var text = Encoding.ASCII.GetString(data);
+                    bool isNotAscii = HasNonASCIIChars(text);
+                    if (isNotAscii)
+                    {
+                        OutputTextBox.Text = "Invalid binary string";
+                        return;
+                    }
+                    else
+                    {
+                        string newResult = text;
+                        result = string.Join(" ", result, newResult);
+                    }
+
+                }
+                OutputTextBox.Text = result;
+                return;
+            }
+           else
+            {
+                var binary = ToBinary(ConvertToByteArray(InputTextBox.Text, Encoding.ASCII));
+                OutputTextBox.Text = binary;
+            }
+        }
+    }
+}
